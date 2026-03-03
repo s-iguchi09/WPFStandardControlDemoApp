40baa8df2233cccbcd67a95b0cc1bf2a7afbec80
s-iguchi09 <stand.alone.complex.laughingman@gmail.com>
2026-03-03 22:55:22 +0900

    誤字修正

---
+++ src/WPFStandardControlDemoApp/Common/Helpers/OrientationHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace WPFStandardControlDemoApp.Common.Helpers
+{
+    public static class OrientationHelpers
+    {
+        public static IEnumerable<Orientation> AllOrientations => Enum.GetValues<Orientation>().Cast<Orientation>();
+    }
+}
