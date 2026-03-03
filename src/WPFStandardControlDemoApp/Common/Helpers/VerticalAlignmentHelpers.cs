63e9203aa7e40cb44eb5c25a4a6d1a960978316b
s-iguchi09 <stand.alone.complex.laughingman@gmail.com>
2026-03-03 23:08:03 +0900

    CheckBox,RadioButtonにVerticalCenterAlignmentの項目を追加。

---
+++ src/WPFStandardControlDemoApp/Common/Helpers/VerticalAlignmentHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WPFStandardControlDemoApp.Common.Helpers
+{
+    public class VerticalAlignmentHelpers
+    {
+        public static IEnumerable<VerticalAlignment> AllVerticalAlignments => Enum.GetValues<VerticalAlignment>();
+    }
+}
